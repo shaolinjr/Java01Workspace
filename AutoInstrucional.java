97e6f846145503eaa1e464f8080e727fc9aee670
shaolinjr <arthurclp@icloud.com>
2016-06-03 17:23:11 -0300

    Finalização do AutoInstrucional

--- AutoInstrucional.java
+++ AutoInstrucional.java
@@ -6,90 +6,70 @@ public class AutoInstrucional {
 		Scanner leia = new Scanner(System.in);
 		// Formula para decifrar: M = (M x C) x C^-1
 		
-		// DONE Array alfabeto para gerar a resposta da mensagem
-		// DONE Matriz para conter a matriz codigo
-		// DONE Matriz para conter a matriz chave
-		// DONE Matriz para conter mensagem
-		
 		// codigo de teste: 29 49 -66 49 91 -122 25 31 -55
-		// chave de teste: -1 -2 3 2 3 -5 2 4 5
+		// chave de teste: -1 -2 3 2 3 -5 2 4 -5
 		
 		// chave para decifrar a mensagem ( C )
 		// usuario deve digitar a chave
-		int chave 				[][] 	= new int[3][3];
-		int chaveToNumbersArray [] 		= new int[9];
-		int determinante = 0;
-		int diagonalPrinc = 1;
-		int diagonalSec = 1;
+		int 	chave 				[][] 	= new int[3][3],
+				chaveToNumbersArray [] 		= new int[9],
+				determinante = 0,
+				diagonalPrinc = 1,
+				diagonalSec = 1,
+				msg = 0;
 		
 		// matriz de cofatores de C 
-		int cofatores [][] = new int[3][3];
+		double cofatores [][] = new double[3][3];
 		
 		// matriz mensagem ( M )
 		int mensagem 		[][] = new int[3][3];
 		
 		// matriz codificada ( M x C )
-		int mensagemCodificada 	[][] = new int[3][3];
+		double mensagemCodificada 	[][] = new double[3][3];
 		String mensagemStringArray [];
+		String chaveStringArray [];
 		int mensagemToNumbersArray [] = new int[9];
 		
 		// array resposta
-		char resposta 	[];
+		char resposta[] = new char[9];
 		
 		// array com as letras para formar a mensagem
-		char alfabeto	[] 		= {'#','A','B','C','D','E','F','G','H','I','J',
-								'K','L','M','N','O','P','Q','R','S',
-								'T','U','V','W','X','Y','Z'};
+		char alfabeto	[] 		= {	'#','A','B','C','D','E','F','G','H','I','J',
+									'K','L','M','N','O','P','Q','R','S',
+									'T','U','V','W','X','Y','Z'	};
 		int i = 0, line, column;
-		// Entrada de dados
 		
-		// Entrada do código
-		// metodo para criar o array de mensagem simples
+		// ENTRADA DE DADOS
 		
+		// ENTRADA DA MENSAGEM CODIFICADA
+		
+		// Método para criar o array de mensagem simples
 		System.out.print("Digite o código interceptado: ");
 		mensagemStringArray = leia.nextLine().split(" ");
 		
-		// validação do codigo digitado
+		// Validação do codigo digitado
 		while(mensagemStringArray.length != 9){
 			System.out.print("Código inválido, insira novamente: ");
 			mensagemStringArray = leia.nextLine().split(" ");
+		}
+		
+		// Vamos criar a matriz chave
+		System.out.print("Digite agora a chave da mensagem (números separados por espaço): ");
+		chaveStringArray = leia.nextLine().split(" ");
+		
+		// Validação da chave digitado
+		while(mensagemStringArray.length != 9){
+			System.out.print("Código inválido, insira novamente: ");
+			chaveStringArray = leia.nextLine().split(" ");
 		}
 		
 		// transformamos o Array de tipo String no nosso array do tipo int
 		for (int j = 0; j <= 8; j++){
 			mensagemToNumbersArray[j] = Integer.parseInt(mensagemStringArray[j]);
-		}
-		
-		// criamos a matriz codificada ( M x C )	
-		
-		// linha
-		for (line = 0; line < 3; line++){
-			//coluna
-			for(column = 0; column < 3; column++){
-				
-				mensagemCodificada[line][column] = mensagemToNumbersArray[i];
-				i++;
-
-			}
-		}
-		
-		// Vamos criar a matriz chave
-		
-		System.out.print("Digite agora a chave da mensagem (números separados por espaço): ");
-		mensagemStringArray = leia.nextLine().split(" ");
-		
-		while(mensagemStringArray.length != 9){
-			System.out.print("Código inválido, insira novamente: ");
-			mensagemStringArray = leia.nextLine().split(" ");
-		}
-		
-		// transformamos o Array de tipo String no nosso array do tipo int
-		for (int j = 0; j <= 8; j++){
-			chaveToNumbersArray[j] = Integer.parseInt(mensagemStringArray[j]);
+			chaveToNumbersArray[j] = Integer.parseInt(chaveStringArray[j]);
 		}
 				
 		// criamos a matriz chave ( C )	
-		
 		// redeclaramos o i para resetar o contador do indice do array de chaves
 		i = 0;
 		// linha
@@ -97,65 +77,68 @@ public class AutoInstrucional {
 			//coluna
 			for(column = 0; column < 3; column++){
 				
-				chave[line][column] = chaveToNumbersArray[i];
-				
+				mensagemCodificada[line][column] = mensagemToNumbersArray[i];
+				chave[line][column] = chaveToNumbersArray[i];				
 				i++;
 			}
 		}
 		
-		
-		// Matriz ( M x C )
-		// GUARDAR CODIGO PARA MULTIPLICAÇAO CASO FOR NECESSARIA DEPOIS, MAS JA DIGITAMOS A MENSAGEM CODIFICADA NO INICIO DO PROGRAMA
-		// POR ISSO N É PRECISO MULTIPLICAR
-		// Nesse ponto já temos a matriz de mensagem e de chave criadas, agora temos que multiplicar linha x coluna
-		
-		// linha
-//		for (int line = 0; line < 3; line++){
-//			//coluna
-//			for (int column = 0; column < 3; column++){
-//				// realizando as multiplicações de linha x coluna
-//				for (int t = 0; t < 3; t++){
-//					codigo[line][column] += (mensagemCodificada[line][t]*chave[t][column]);
-//				}
-//			}
-//		}
-		
-		
 		// calculando o determinante
-		// determinante hardcoded
-		// Procurar melhor maneira para calcular o determinante..
 		for (int princ = 0; princ < 3; princ++){
 			diagonalPrinc *= chave[princ][princ];			
 		}
+		
 		diagonalSec *= chave[0][2]*chave[1][1]*chave[2][0];
 		
 		determinante = (diagonalPrinc 	+ (chave[0][1]*chave[1][2]*chave[2][0]) + (chave[0][2]*chave[2][1]*chave[1][0])) - 
 					   (diagonalSec 	+ (chave[0][1]*chave[1][0]*chave[2][2]) + (chave[0][0]*chave[2][1]*chave[1][2]));
 		
-		
-		System.out.println("Diagonal Princ: "	+ diagonalPrinc);
-		System.out.println("Diagonal Sec: "		+ diagonalSec);
-		System.out.println("Determinante: "		+determinante);
+//		System.out.println("Diagonal Princ: "	+ diagonalPrinc);
+//		System.out.println("Diagonal Sec: "		+ diagonalSec);
+//		System.out.println("Determinante: "		+ determinante);
 		
 		// Verificar det != 0 para que seja possível qualquer a matriz inversa da chave
-		
 		if (determinante == 0){
 			System.out.println("Determinante de Matriz Chave = 0, chave inválida");
-			// o que fazer depois?
-			// teoricamente deveríamos parar o programa, ou então perguntar se o usuário deseja digitar uma nova chave..
 		}
-		
-		// calcular matriz de cofatores de C
-		// Funcionamento matematico
-		/*	a[0][0] ^-1 = a[1][1]*a[2][2] - a[1][2] * a[2][1]
-		 * */
-		
+		else {
 			
-		
+			// calculando os cofatores e transpondo ao mesmo tempo
+			cofatores[0][0] = chave[1][1] * chave[2][2] - chave[1][2] * chave[2][1];
+			cofatores[1][0] = -(chave[1][0] * chave[2][2] - chave[1][2] * chave[2][0]);
+			cofatores[2][0] = chave[1][0] * chave[2][1] - chave[1][1] * chave[2][0];
+			
+			cofatores[0][1] = -(chave[0][1] * chave[2][2] - chave[0][2] * chave[2][1]);
+			cofatores[1][1] = chave[0][0] * chave[2][2] - chave[0][2] * chave[2][0];
+			cofatores[2][1] = -(chave[0][0] * chave[2][1] - chave[0][1] * chave[2][0]);
+			
+			cofatores[0][2] = chave[0][1] * chave[1][2] - chave[0][2] * chave[1][1];
+			cofatores[1][2] = -(chave[0][0] * chave[1][2] - chave[0][2] * chave[1][0]);
+			cofatores[2][2] = chave[0][0] * chave[1][1] - chave[0][1] * chave[2][0];
+			
+			// for para 
+			for (line = 0; line < 3; line++){
+				
+				//coluna
+				for(column = 0; column < 3; column++){
+					
+					cofatores[line][column] = ((double) 1 / determinante) * cofatores[line][column];
+					//multiplicacao da matriz de cofatores transposta pela matriz codificada
+					for (int t = 0; t < 3; t++){
+						mensagem[line][column] += (mensagemCodificada[line][t] * cofatores[t][column]);
+					}
+//					System.out.println((line + 1)+" "+(column + 1)+ ": "+mensagem[line][column]);						
+					// Adicionando os caracteres no array de resposta
+					resposta[msg] = alfabeto[mensagem[line][column]];
+					msg++;
+				}	
+			}
+		}
+		leia.close();
+		// Mostrando o resultado
+		System.out.println("\nMensagem decriptada: \n");
+		for (int show = 0; show < resposta.length; show++){
+			System.out.print(resposta[show]);
+		}
 	}
-		
-		
-		
-
-
 }
